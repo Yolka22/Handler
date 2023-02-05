7ef807e4420145f266784ddb8b54d22e9d422f65
Yehor <egorkorsun7@gmail.com>
2023-02-05 15:01:41 +0100

    Добавьте файлы проекта.

---
+++ Handler/Program.cs
@@ -0,0 +1,64 @@
+﻿using static System.Console;
+
+namespace Handler
+{
+    internal class Program
+    {
+        private class Abstract_Handler
+        {
+            protected string type;
+
+            public virtual void Open()
+            { WriteLine($"файл {type} открыт"); }
+
+            public virtual void Create()
+            { WriteLine($"файл {type} создан"); }
+
+            public virtual void Change()
+            { WriteLine($"файл {type} изменён"); }
+
+            public virtual void Save()
+            { WriteLine($"файл {type} сохранен"); }
+        }
+
+        private class XMLHandler : Abstract_Handler
+        {
+            public XMLHandler()
+            {
+                type = "XML";
+            }
+        }
+
+        private class TXTHandler : Abstract_Handler
+        {
+            public TXTHandler()
+            {
+                type = "TXT";
+            }
+        }
+
+        private class DOCHandler : Abstract_Handler
+        {
+            public DOCHandler()
+            {
+                type = "DOC";
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            Abstract_Handler[] handlers = { new TXTHandler(), new XMLHandler(), new DOCHandler() };
+
+            for (int i = 0; i < handlers.Length; i++)
+            {
+                handlers[i].Create();
+                handlers[i].Open();
+                handlers[i].Change();
+                handlers[i].Save();
+                WriteLine();
+            }
+
+            ReadLine();
+        }
+    }
+}
